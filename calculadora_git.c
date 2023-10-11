1c55794f0a4ae097484bbe5867d4a5e9579fd09a
DIEGO FRANCISCO SALDAÑA CERON <dsaldanaceron@gmail.com>
2023-10-10 19:46:21 -0600

    Se hizo un munu de seleccion, se integro factorial, factorial recursivo y euler

--- calculadora_git.c
+++ calculadora_git.c
@@ -7,21 +7,31 @@ int suma(int a, int b);
 int multiplicacion(int a, int b);
 int resta(int a, int b);
 double division(int a, int b);
+int factorial(int a);
+int factorial_rec(int a);
+double euler(int a);
 //main
 int main (void)
 {
 //Variables
   int num1,num2;
   char c;
+  double db;
   int numeros_caso_a[10];
   int res = 0;
 //Procesos
-  printf("\n\n¿Que tipo de operación quieres hacer? [+,-,*,/]\n ");
+  printf("\n\t\t***********-MENU-***********\n");
+  printf("\t\t| + (SUMA )     -  (RESTA) |\n");
+  printf("\t\t| * (MULTi)     /  (DIVIS) |\n");
+  printf("\t\t| f (facto)     F  (FACTO) |\n");
+  printf("\t\t| e (EULER)                |\n");
+  printf("\t\t****************************\n");
+  printf("¿Que tipo de operación quieres hacer?\n ");
   scanf("%c", &c);
   switch(c){
       case '+':
       {
-	    printf("\n\nSuma");
+	    printf("\nSuma");
 	    printf("\nIngresa dos numeros: [n1,n2]\n");
 	    scanf("%d,%d",&num1,&num2);
 	    res=suma(num1,num2);
@@ -30,7 +40,7 @@ int main (void)
       
       case '-':
       {
-	    printf("\n\nResta");
+	    printf("\nResta");
 	    printf("\nIngresa dos numeros: [n1,n2]\n");
 	    scanf("%d,%d",&num1,&num2);
 	    res=resta(num1,num2);
@@ -39,26 +49,53 @@ int main (void)
       
       case '/':
       {
-	    printf("\n\nDivisión");
+	    printf("\nDivisión");
 	    printf("\nIngresa dos numeros(n1/n2): [n1,n2]\n");
 	    scanf("%d,%d",&num1,&num2);
-	    res=division(num1,num2);
+	    db=division(num1,num2);
+	    printf("Tu respuesta para %c :   %f", c, db);
 	    break;
       }
       
       case '*':
       {
-	    printf("\n\nMultpiplicación");
+	    printf("\nMultpiplicación");
 	    printf("\nIngresa dos numeros: [n1,n2]\n");
 	    scanf("%d,%d",&num1,&num2);
 	    res=multiplicacion(num1,num2);
 	    break;
       }
 
+      case 'f':{//factorial
+            printf("\nFactorial");
+            printf("\nIngresa un numero:\n");
+            scanf("%d",&num1);
+            res=factorial(num1);
+	    printf("Tu respuesta para %c :   %d", c, res);
+            break;
+      }
+	
+      case 'F':{//factorial recursion
+            printf("\nFactorial");
+            printf("\nIngresa un numero:\n");
+            scanf("%d",&num1);
+            res=factorial_rec(num1);
+	    printf("Tu respuesta para %c :   %d", c, res);
+            break;
+      }
+      case 'e':{//factorial recursion
+            printf("\nEuler");
+            printf("\nIngresa un numero:\n");
+            scanf("%d",&num1);
+            db=euler(num1);
+	    printf("Tu respuesta para %c :   %f", c, db);
+            break;
+      }
+	
       case 'a':
       {
         leer_arreglo(numeros_caso_a);
-        printf("\n\n");
+        printf("\n");
         res = sumar_arreglo(numeros_caso_a);
 
         printf("Tu respuesta para %c:   %d", c, res);
@@ -67,7 +104,7 @@ int main (void)
     }
 
 //FIN DEL PROGRAMA
-  printf("Tu respuesta para %c :   %d", c, res);
+  
   printf("\n\n\tPrograma Finalizado con ÉXTO\n\n");
   return 0;
 }
@@ -93,6 +130,41 @@ double division(int a, int b){
   dv=(double)a;
   return dv;
 }
+int factorial(int a){
+  int i, f;
+  f=1;
+  for(i=1;i<=a;i++){
+     f=f*i;
+  }
+  return f;
+}
+
+int factorial_rec(int a){
+  int fr;
+  if(a == 0){
+    return 1;
+  }
+  else{
+    fr=factorial_rec(a-1);
+    return a*fr;
+  }
+}
+
+double euler(int a){
+  int n,i,f,de;
+  double eu, db;
+  n=a;
+  eu=1;
+  for(i=1;i<a;i++){
+    f=factorial(i);
+    db=(double)f;
+    eu=eu+(1/db);
+  }
+  return eu;
+}
+
+
+
 //funciones arreglos
 void leer_arreglo(int num[])
 {
